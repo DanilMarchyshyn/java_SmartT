9d2ba2794d16895a400871221f45d46831c5ed52
dmariks <dmariks@gmail.com>
2020-08-19 11:49:39 +0300

    add int l = *
    Додав переменную l

--- sandbox/src/main/java/ua/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ua/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,7 +3,13 @@ package ua.stqa.pft.sandbox;
 public class MyFirstProgram {
 
     public static void main(String[] args) {
-        System.out.println("Hello, Sveta!");
+        System.out.println("Hello, " + "Sveta!");
+
+        int l = 17;
+        System.out.println("Площадь квадрата " + l + " = " + (l * l));
     }
+
+
+
 
 }
