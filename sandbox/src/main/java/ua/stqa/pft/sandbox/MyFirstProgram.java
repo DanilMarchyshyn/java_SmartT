ece5b56d56e7e620240310e28c96030d8c0e77fd
dmariks <dmariks@gmail.com>
2020-08-19 12:24:41 +0300

    add String and double
    Додав типи переменных
    string - строка
    double - плавающая точка 8.1
    int - целоме число

--- sandbox/src/main/java/ua/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ua/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,10 +3,13 @@ package ua.stqa.pft.sandbox;
 public class MyFirstProgram {
 
     public static void main(String[] args) {
-        System.out.println("Hello, " + "Sveta!");
+        String somebody = "Sveta";
+        System.out.println("Hello, " + somebody + "!");
 
         int l = 17;
-        System.out.println("Площадь квадрата " + l + " = " + (l * l));
+        double d = 8.01;
+        int e = l * l;
+        System.out.println("Площадь квадрата " + l + " = " + e);
     }
 
 
