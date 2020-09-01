52415e4402405067dc553330974b7fdc9dcc801a
dmariks <dmariks@gmail.com>
2020-09-01 17:37:17 +0300

    переделал классы под area (lesson 15)

--- sandbox/src/main/java/ua/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ua/stqa/pft/sandbox/MyFirstProgram.java
@@ -8,23 +8,17 @@ public class MyFirstProgram {
         hello("Sveta");
 
         Square s = new Square(7);
-        System.out.println("Площадь квадрата со сторной " + s.l + " = " + area(s));
+        System.out.println("Площадь квадрата со сторной " + s.l + " = " + s.area());
 
         Rectangle r = new Rectangle(8, 7);
-        System.out.println("Площадь прямоугольника со сторнами " + r.a + " и " + r.b +" = " + area(r));
+        System.out.println("Площадь прямоугольника со сторнами " + r.a + " и " + r.b +" = " + r.area());
     }
 
     public static void hello(String somebody) {
         System.out.println("Hello, " + somebody + "!");
     }
 
-    public static double area (Square s) {
-        return s.l  * s.l;
-    }
 
-    public static double area (Rectangle r) {
-        return r.a * r.b;
-    }
 
 
 }
